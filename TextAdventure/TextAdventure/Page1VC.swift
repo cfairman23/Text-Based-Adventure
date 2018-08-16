e3bd88e2f7a881eefcf058a79ee19da48de8dcef
Connor Fairman <cfairman@seas.upenn.edu>
2018-08-16 19:42:56 -0400

    nav controller and pushing first test view

---
+++ TextAdventure/TextAdventure/Page1VC.swift
@@ -0,0 +1,21 @@
+//
+//  Page1VC.swift
+//  TextAdventure
+//
+//  Created by Connor Fairman on 8/16/18.
+//  Copyright © 2018 connorfairman. All rights reserved.
+//
+
+import UIKit
+
+class Page1VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
